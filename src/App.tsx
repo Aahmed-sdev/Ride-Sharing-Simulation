f874d2e1bd6f93204cd8a2cd9257c5a4e373c8d5
Aahmed <aahmed.sde@gmail.com>
2024-01-24 01:06:44 +0530

    React migration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to Ride-Sharing-Simulation
+    </div>
+  );
+}
+
+export default App;
